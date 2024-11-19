a39c1ddc9ff47ac1a61592006fd56e16d758321e
baraa-plus <baraa.jaber.dev@gmail.com>
2024-11-19 13:20:43 +0300

    Update two_sum.py

--- Leetcode/two_sum.py
+++ Leetcode/two_sum.py
@@ -5,3 +5,12 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+# Method-2:
+class Solution:
+    def twoSum(self, nums, target: int):
+        dict = {}
+        for i in range(len(nums)):
+            num = target - nums[i]
+            if num in dict:
+                return [dict[num], i]
+            dict[nums[i]] = i
